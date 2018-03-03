d47ca67024010d98549e76c76be5569d5d875ac8
Greifrut <artur.bunko@gmail.com>
2018-03-03 15:28:00 +0300

    начало friendship

--- app/models/frendship.rb
+++ app/models/frendship.rb
@@ -1,2 +1,6 @@
 class Frendship < ApplicationRecord
+    belongs_to :requester, class: "User"
+    belongs_to :requested, class: "User"
+    validates :requester_id, presence: true
+    validates :requested_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+    #/Autchlogic 
     acts_as_authentic
+    #/
+
+    has_many :friendships, foreign_key: "requester_id", dependent: :destroy
+    has_many :reverse_freindships, foreign_key: "requested_id",
+                                   class_name: "Friendship",
+                                   dependent: :destroy
+    has_many :requesters, through: :reverse_freindships, source: :requester
 end
