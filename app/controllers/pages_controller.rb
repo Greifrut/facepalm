98c2f179bc4d6ffdeafe5e836271cd3f4f471e81
Greifrut <artur.bunko@gmail.com>
2018-03-12 16:27:19 +0300

    работа над сообщениями

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
 
     def index
       @post = current_user.posts.build if current_user
+      @feed = current_user.posts.paginate(page: params[:page])
     end
 end
