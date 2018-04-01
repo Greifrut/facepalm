c89b5a8e21a9677192c23cc0f22f646f4ee4743a
Greifrut <artur.bunko@gmail.com>
2018-04-01 16:47:13 +0300

    post layouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
     before_action :current_user_session
+    layout "post", only: [:show]
+
+    def show
+      @post = Post.find(params[:id])
+    end
 
     def new
       @post = Post.new
+    end
+
+    def index
+      @posts = current_user.posts.paginate(page: params[:page])
     end
 
     def create
@@ -15,9 +24,25 @@ class PostsController < ApplicationController
         redirect_to current_user
       end
     end
+    
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find(params[:id])
+      if @post.update_attributes(post_params)
+        flash[:succes] = "Successfully created..."
+        redirect_to posts_path(current_user)
+      else
+        flash[:danger] = "Error"
+      end
+    end
 
     def destroy
-      
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path(current_user)
     end
 
     private
