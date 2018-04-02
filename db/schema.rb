547107b80fee9fb5f6b2c52945ebd949733e501e
Greifrut <artur.bunko@gmail.com>
2018-04-02 12:35:58 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401112206) do
+ActiveRecord::Schema.define(version: 20180402093535) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20180401112206) do
     t.string "last_login_ip"
     t.string "name"
     t.string "surname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
