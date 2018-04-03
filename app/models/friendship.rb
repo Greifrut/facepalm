313bbf4e7cb05aece2b104daac25046fe7a198dd
Greifrut <artur.bunko@gmail.com>
2018-04-03 16:37:45 +0300

    edit

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,4 @@
 class Friendship < ApplicationRecord
+    belongs_to :user
+    belongs_to :friend, class_name: "Friend", foreign_key: :friend_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,8 @@ class User < ApplicationRecord
     #
     mount_uploader :avatar, AvatarUploader
     #Зависимости для Friendship < User
-    has_many :active_friendships, class_name: "Friendship", foreign_key: "requester_id", dependent: :destroy
-    has_many :pasive_friendships, class_name: "Friendship", foreign_key: "requested_id", dependent:  :destroy
-   
-    has_many :friends, -> {where("active_friendships.accepted" == true)}, through: :active_friendships, source: :requested
-    has_many :inverse_friends, -> {where("active_friendships.accepted" == true)}, through: :pasive_friendships, source: :requester
-
-    has_many :sent_requests, -> {where("active_friendships.accepted" == false)}, through: :active_friendships, source: :requested
-    has_many :requests_received, -> {where("active_friendships.accepted" == false)}, through: :pasive_friendships, source: :requester
+    
+    
 
     #Зависимость Post < User
     has_many :posts, dependent: :destroy
@@ -23,21 +17,8 @@ class User < ApplicationRecord
     has_many :comments, dependent: :destroy
 
 
-    def is_friend?(other_user)
-        other_user.in?(friends) || other_user.in?(inverse_friends)
-    end
+    #Методы для Friendships
 
-    def find_accepted_friendships?(other_user)
-        active_friendships.where(accepted: true).limit(1).first
-    end
-
-    def request_sent(other_user)
-        active_friendships.find_by(requester_id: other_user, accepted: false)
-    end
-
-    def request_received(other_user)
-        pasive_friendships.find_by(requester_id: other_user, accepted: false)
-    end
 
 
 
