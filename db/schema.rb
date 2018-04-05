5e85b61afdb8322cb217581e45c45f3f525108b3
Grefrut <artur.bunko@gmail.com>
2018-04-05 13:22:20 +0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403135710) do
+ActiveRecord::Schema.define(version: 20180405102109) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -20,22 +20,12 @@ ActiveRecord::Schema.define(version: 20180403135710) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "friend_requests", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "friend_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friend_requests_on_friend_id"
-    t.index ["user_id"], name: "index_friend_requests_on_user_id"
-  end
-
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
+    t.boolean "accepted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
